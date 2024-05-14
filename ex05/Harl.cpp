3676973ff52ef6e3d3e115aa46b54995ed459058
Juiceee <lbehr@student.42.fr>
2024-05-14 16:03:25 +0200

    oui

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -20,25 +20,25 @@ int	Harl::strtonb(std::string str)
 
 void Harl::debug(void)
 {
-	std::cout << "[DEBUG]: ";
+	std::cout << "[DEBUG]\n";
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !\n";
 }
 
 void Harl::info(void)
 {
-	std::cout << "[INFO]: ";
+	std::cout << "[INFO]\n";
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !\n";
 }
 
 void Harl::warning(void)
 {
-	std::cout << "[WARNING]: ";
+	std::cout << "[WARNING]\n";
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
 }
 
 void Harl::error(void)
 {
-	std::cout << "[ERROR]: ";
+	std::cout << "[ERROR]\n";
 	std::cout << "This is unacceptable ! I want to speak to the manager now.\n";
 }
 
