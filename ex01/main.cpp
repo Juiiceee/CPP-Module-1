f959c5bdf751303819792d7bc539861fa8efd1e0
Juiceee <lbehr@student.42.fr>
2024-05-19 15:06:55 +0200

    add mieux

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,10 +1,17 @@
 #include "Zombie.hpp"
+#include <iostream>
 
 int main()
 {
-	int N = 10;
-	Zombie *zombie = zombieHorde(N, "fesse");
+	int N = 100;
+	Zombie *zombie = zombieHorde(N, "Feur");
+	if (!zombie)
+	{
+		std::cout << "Nombre rentre inferieur a 1\n";
+		return (1);
+	}
 	for (int i = 0; i < N; i++)
 		zombie[i].announce();
 	delete[] zombie;
+	return (0);
 }
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -2,7 +2,14 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
+	if (N < 1)
+		return (NULL);
 	Zombie *zombie = new Zombie[N];
+	if (!zombie)
+	{
+		std::cout << "Allocation n'a pas fonctionne\n";
+		return (NULL);
+	}
 	for (int i = 0;i < N; i++)
 		zombie[i].change_name(name);
 	return (zombie);
